2fb33f511f74d6833832a82d0b65f6574c35640b
kelvinkirima014 <kelvinkirima2@gmail.com>
2022-05-04 06:59:01 +0300

    first commit

---
+++ cfprogram/target/debug/deps/cfprogram-61df3c6c8d91dcc1.d
@@ -0,0 +1,5 @@
+/home/kelvin/crowdfunding/cfprogram/target/debug/deps/cfprogram-61df3c6c8d91dcc1.rmeta: src/lib.rs
+
+/home/kelvin/crowdfunding/cfprogram/target/debug/deps/cfprogram-61df3c6c8d91dcc1.d: src/lib.rs
+
+src/lib.rs:
---
+++ cfprogram/target/debug/deps/cfprogram-fffe0665b891e2a7.d
@@ -0,0 +1,5 @@
+/home/kelvin/crowdfunding/cfprogram/target/debug/deps/cfprogram-fffe0665b891e2a7.rmeta: src/lib.rs
+
+/home/kelvin/crowdfunding/cfprogram/target/debug/deps/cfprogram-fffe0665b891e2a7.d: src/lib.rs
+
+src/lib.rs:
