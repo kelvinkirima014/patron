d2362af6a1b494ac0df47194b28234a5f4aba6fe
kelvinkirima014 <kelvinkirima2@gmail.com>
2022-05-07 18:41:17 +0300

    iterate over accounts

--- cfprogram/src/lib.rs
+++ cfprogram/src/lib.rs
@@ -70,6 +70,8 @@ fn create_campaign(
     accounts: &[AccountInfo],
     instruction_data: &[u8],
 ) -> ProgramResult {
+    //iterator on accounts
+    let accounts_iter = &mut accounts.iter();
 
     Ok(())
 
