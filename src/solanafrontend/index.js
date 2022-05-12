9d0d1fd715744c5b2913dca4111ed36be383c9a5
kelvinkirima014 <kelvinkirima2@gmail.com>
2022-05-12 16:32:05 +0300

    add getBlockHash fn

--- src/solanafrontend/index.js
+++ src/solanafrontend/index.js
@@ -7,3 +7,22 @@ import {
     TransactionInstruction
 } from "@solana/web3.js";
 import { deserialize, serialize } from "borsh";
+
+const cluster = "https://api.devnet.solana.com";
+const connection = new Connection(cluster, "cluster");
+const wallet = new Wallet("https://www.sollet.io", cluster);
+const programId = new PublicKey(
+	"3LyMjj91nSSQcmNA7h9RZ2N2sb74w4jMx67tRoTCUrT7"
+);
+
+export async function setPayerAndBlockhashTransaction(instructions){
+	const transaction = new Transaction();
+	instructions.forEach(element => {
+		transaction.add(element);
+	});
+
+	transaction.feePayer = wallet.publicKey;
+	let hash = await connection.getLatestBlockhash();
+	transaction.recentBlockhash = hash.blockhash;
+	return transaction;
+}
