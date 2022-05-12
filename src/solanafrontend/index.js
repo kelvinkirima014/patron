fdf4f3aa80166d829ce1ebdeb473845b4625dfe4
kelvinkirima014 <kelvinkirima2@gmail.com>
2022-05-12 16:17:27 +0300

    add solana frontend

---
+++ src/solanafrontend/index.js
@@ -0,0 +1,9 @@
+import Wallet from "@project-serum/sol-wallet-adapter";
+import {
+    Connection,
+    SystemProgram,
+    Transaction,
+    PublicKey,
+    TransactionInstruction
+} from "@solana/web3.js";
+import { deserialize, serialize } from "borsh";
